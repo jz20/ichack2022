7ca9bf0717b2669b4070af6ab996fc01de62538e
elsa-polo <elsa.polo.laube@gmail.com>
2022-02-05 20:40:39 +0000

    front bit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Form } from 'react-bootstrap';
+import { useState } from "react";
+import Spotify from 'react-spotify-embed';
+
 
 function App() {
+
+  const [text, setText] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <section>
+      <span className="h1">
+        Hello
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <br></br>
+          <Form.Control 
+          as="textarea" 
+          rows={3} 
+          placeholder="How was your day??"
+          onChange={(event) => setText(event.target.value)}
+          />
+          <br></br>
+          <Button 
+          type="submit" 
+          className="my-2"
+          onClick={console.log(text)}>
+            Find my playlist
+          </Button>
+        </Form.Group>
+      </span>
+      <br></br>
+      <Spotify link="https://open.spotify.com/playlist/6PESRz1MZWlElXYHkTAvqB?si=3db00481f37344fe" />
+  </section>
   );
 }
 
