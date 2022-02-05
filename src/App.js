e15a968b5707dc2207eaa4bb34f3c204c215fc40
elsa-polo <elsa.polo.laube@gmail.com>
2022-02-05 21:14:20 +0000

    working (ugly) font end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Form } from 'react-bootstrap';
 import { useState } from "react";
 import Spotify from 'react-spotify-embed';
 
@@ -9,27 +8,27 @@ function App() {
 
   const [text, setText] = useState("");
 
+  const doSubmit = async (event) => {
+    console.log("Got text");
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const text = formData.get("textarea");
+    console.log(text);
+;
+  };
+
   return (
   <section>
       <span className="h1">
         Hello
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
+        <form onSubmit={doSubmit}>
+        <label>
+          How was your day?
           <br></br>
-          <Form.Control 
-          as="textarea" 
-          rows={3} 
-          placeholder="How was your day??"
-          onChange={(event) => setText(event.target.value)}
-          />
-          <br></br>
-          <Button 
-          type="submit" 
-          className="my-2"
-          onClick={console.log(text)}>
-            Find my playlist
-          </Button>
-        </Form.Group>
+          <input type="text" name="textarea" value={text} onChange={(event) => setText(event.target.value)}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       </span>
       <br></br>
       <Spotify link="https://open.spotify.com/playlist/6PESRz1MZWlElXYHkTAvqB?si=3db00481f37344fe" />
