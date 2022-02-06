3d4614bd38f44af2e58dab5a23a0f41f4826c59c
nataliewutc <nataliewu.261201@gmail.com>
2022-02-06 09:20:42 +0000

    fixed if emotion not found

--- server/main.py
+++ server/main.py
@@ -22,7 +22,7 @@ sp = spotipy.Spotify(auth=token)
 list_of_songs = []
 
 SENTIMENT_TO_PLAYLIST ={
-    "anger": "2G5inP1EjUb0XK6sEELY0l", "anticipation": "4u4cdIly29BKmrqQClLN2b", "disgust": "7IbEby4yEjlKX9J6tmUf7H", "fear": "739d1Ze6FTBIrxCbNYLM24", "joy": "1p5p2amD3Xuoc64vhg3hv2", "negative": "4sHGDdKN4k9UXCHwG8v5Nj", "positive": "0Pj4JFHttRgW2RKMjvTjGU", "sadness": "6s6jsuowm8DKoeGG3aBOou", "surprise": "3jQhgOxSDeu5GB0SJt2mJg", "trust": "6PESRz1MZWlElXYHkTAvqB"
+    "anger": "2G5inP1EjUb0XK6sEELY0l", "anticipation": "4u4cdIly29BKmrqQClLN2b", "disgust": "7IbEby4yEjlKX9J6tmUf7H", "fear": "739d1Ze6FTBIrxCbNYLM24", "joy": "1p5p2amD3Xuoc64vhg3hv2", "negative": "4sHGDdKN4k9UXCHwG8v5Nj", "positive": "0Pj4JFHttRgW2RKMjvTjGU", "sadness": "6s6jsuowm8DKoeGG3aBOou", "surprise": "3jQhgOxSDeu5GB0SJt2mJg", "trust": "6PESRz1MZWlElXYHkTAvqB", "random": "3vfMDL3goMDkiUDHUjcuPr"
 }
 
 
@@ -52,7 +52,7 @@ def get_sentiment(sentence):
                 sentiment[emotion] += score
 
     if not sentiment:
-        return 'none'
+        return 'random'
 
     ranked = sorted(sentiment.items(), key=lambda x: x[1])
     top_score = ranked[0][1]
